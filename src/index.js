4bb3087fa1452caed561b73a70b98e6531cc9b32
faisal.khan <faisal.khan@bookmyshow.com>
2021-03-12 14:25:19 +0530

    Add css support and sass support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss'
+
 const pokemons = {
     electric: "pikachu",
     water: "squartle",
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-
+    target: process.env.NODE_ENV === 'production' ? 'browserslist' : 'web', // hot reload bug on webpack due to which we have done this
     module: {
         rules: [
             {
@@ -11,13 +11,20 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
             }
         ]
     },
+
+    plugins: [new MiniCssExtractPlugin()],
 
     devtool: "source-map",
 
     devServer: {
         contentBase: "./dist",
+        hot: true
     }
 }
